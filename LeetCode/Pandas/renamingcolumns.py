2a73aaacad1be428bc349bf325e443a04f4b9504
Jade Yang <jadejmyang@gmail.com>
2026-05-05 19:34:37 -0500

    Update renamingcolumns.py

--- LeetCode/Pandas/renamingcolumns.py
+++ LeetCode/Pandas/renamingcolumns.py
@@ -7,3 +7,6 @@ def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
     # students.rename(columns={'id':'student_id', 'first':'first_name', 'last':'last_name', 'age':'age_in_years'})
     # the code above does not change the original table. It needs to be assigned to the original dataframe.
     # correct code: students = students.rename(columns={'id':'student_id', 'first':'first_name', 'last':'last_name', 'age':'age_in_years'})
+
+    # or include inplace = True at the end
+    # students.rename(columns={'id': 'student_id', 'first': 'first_name', 'last': 'last_name', 'age': 'age_in_years'}, inplace=True)
