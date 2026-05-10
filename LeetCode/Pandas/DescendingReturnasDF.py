634b5d26f62b5092767b95c7255bfec3de54f698
Jade Yang <jadejmyang@gmail.com>
2026-05-10 17:39:27 -0500

    Create DescendingReturnasDF.py

---
+++ LeetCode/Pandas/DescendingReturnasDF.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    animals = animals.loc[(animals.weight > 100)].sort_values(by='weight', ascending = False)
+    return animals[['name']] # to return as DataFrame
+
+  #improved answer
+  return animals[animals['weight'] > 100].sort_values(['weight'],ascending=False)[['name']]
+
+"""
+Example 1:
+
+Input: 
+DataFrame animals:
++----------+---------+-----+--------+
+| name     | species | age | weight |
++----------+---------+-----+--------+
+| Tatiana  | Snake   | 98  | 464    |
+| Khaled   | Giraffe | 50  | 41     |
+| Alex     | Leopard | 6   | 328    |
+| Jonathan | Monkey  | 45  | 463    |
+| Stefan   | Bear    | 100 | 50     |
+| Tommy    | Panda   | 26  | 349    |
++----------+---------+-----+--------+
+Output: 
++----------+
+| name     |
++----------+
+| Tatiana  |
+| Jonathan |
+| Tommy    |
+| Alex     |
++----------+
+"""
+    
