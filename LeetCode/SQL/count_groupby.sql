9eb4045f461ee655781173931d3d093e09c26e9f
Jeongmin Jade Yang <jadejmyang@gmail.com>
2026-06-09 11:00:52 -0500

    Updated the sql; Use WHERE NOT EXISTS() no need left join

--- LeetCode/SQL/count_groupby.sql
+++ LeetCode/SQL/count_groupby.sql
@@ -4,3 +4,13 @@ FROM Visits AS v
 LEFT JOIN Transactions AS t ON v.visit_id = t.visit_id
 WHERE transaction_id IS NULL -- find customer who didn't make purchase at a visit
 GROUP BY customer_id
+
+-- Best Practice; No LEFT JOIN
+SELECT v.customer_id, count(v.customer_id) as count_no_trans
+FROM visits v
+WHERE NOT EXISTS (
+    select t.visit_id
+    from transactions t
+    where t.visit_id = v.visit_id
+)
+GROUP BY v.customer_id
