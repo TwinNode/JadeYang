b258d5e64979c0d8f93a2d21b79810bc8b9a03d7
Jeongmin Jade Yang <jadejmyang@gmail.com>
2026-06-09 10:54:01 -0500

    Created a sql; Use COUNT() as, GROUP BY

---
+++ LeetCode/SQL/count_groupby.sql
@@ -0,0 +1,6 @@
+SELECT customer_id,
+       COUNT(v.visit_id) AS count_no_trans
+FROM Visits AS v
+LEFT JOIN Transactions AS t ON v.visit_id = t.visit_id
+WHERE transaction_id IS NULL -- find customer who didn't make purchase at a visit
+GROUP BY customer_id
