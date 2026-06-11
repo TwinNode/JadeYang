9a1a236f3787795e0201229f654575becc533abf
Jeongmin Jade Yang <jadejmyang@gmail.com>
2026-06-11 16:14:57 -0500

    Create a sql; use ROUND(AVG( ::numeric, 3))

---
+++ LeetCode/SQL/round_avg_numeric.sql
@@ -0,0 +1,21 @@
+SELECT s.machine_id, ROUND(AVG(e.timestamp-s.timestamp)::numeric, 3)
+FROM Activity AS s
+JOIN Activity AS e
+ON  s.machine_id = e.machine_id
+    AND s.process_id = e.process_id
+    AND s.activity_type = 'start'
+    AND e.activity_type = 'end'
+GROUP BY s.machine_id;
+
+-- Best Practice
+SELECT machine_id, ROUND(
+    AVG(
+        CASE
+            WHEN activity_type = 'start' THEN -timestamp
+            ELSE timestamp
+    END)::decimal * 2 -- two timestamps (start, end)
+, 3) AS processing_time
+FROM Activit
+GROUP BY machine_id
+ORDER BY machine_id;
+
