2c830e4c6dfd4e8a2cb659f105818cf0af8c2baa
Jeongmin Jade Yang <jadejmyang@gmail.com>
2026-06-21 11:09:57 -0500

    modified

--- LeetCode/SQL/activeuser.sql
+++ LeetCode/SQL/activeuser.sql
@@ -1,4 +1,5 @@
 SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE activity_date >= '2019-07-27' :: date - 30 -- :: date change to date type
+WHERE activity_date BETWEEN '2019-07-27' :: date - 29 AND '2019-07-27' :: date 
+-- ::date change to date type
 GROUP BY activity_date
