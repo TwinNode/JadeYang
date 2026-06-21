ad83a1bc090f6fb77b760bbcb2a7a576db3bf41d
Jeongmin Jade Yang <jadejmyang@gmail.com>
2026-06-21 11:23:36 -0500

    Find first year order only WHERE IN () OR MIN(YEAR) SQLITE only

---
+++ LeetCode/SQL/firstyear_order.sql
@@ -0,0 +1,9 @@
+-- ALL DB 
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) IN (SELECT product_id, MIN(year) FROM Sales GROUP BY product_id)
+
+-- ONLY SQLITE
+SELECT product_id, MIN(year) AS first_year, quantity, price
+FROM Sales
+GROUP BY product_id;
